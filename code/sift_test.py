ea14bbb99d6ef7fb58cd679b0a35ba9d2991e82f
yunji <66745172+J-yun-ji@users.noreply.github.com>
2023-12-22 00:47:20 +0900

    Create sift_test.py

---
+++ code/sift_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('test2.jpg')
+gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+sift = cv.SIFT_create()
+kp = sift.detect(gray,None)
+img=cv.drawKeypoints(gray,kp,img)
+cv.imwrite('sift_keypoints2.jpg',img)
+
+img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv.imwrite('sift_keypoints2.jpg',img)
+
+sift = cv.SIFT_create()
+kp, des = sift.detectAndCompute(gray,None)
